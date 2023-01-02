cc0d76a66f06bdfcecc9f903c8672ed0f4a40ce2
Maksym <maksym.levenets@gmail.com>
2023-01-02 18:35:20 +0200

    Chat line is added.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(3000, () => {
